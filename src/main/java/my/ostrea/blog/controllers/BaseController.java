b03bc575f6fe005feaed94125302bb7821de7407
ostrea <localhost@localhost>
2015-11-04 15:16:36 +0300

    Get user articles from db

--- src/main/java/my/ostrea/blog/controllers/BaseController.java
+++ src/main/java/my/ostrea/blog/controllers/BaseController.java
@@ -1,13 +1,24 @@
 package my.ostrea.blog.controllers;
 
+import my.ostrea.blog.models.MyUser;
+import my.ostrea.blog.models.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/")
 public class BaseController {
+
+    @Autowired
+    UserRepository userRepository;
+
     /**
      * Handles '/'
      * @param model
@@ -15,6 +26,10 @@ public class BaseController {
      */
     @RequestMapping
     public String index(Model model) {
+        Optional<MyUser> userFromDb = userRepository
+                .findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        userFromDb.map(user -> model.addAttribute("articles", user.getArticles()));
+
         return "index";
     }
 
