4329a3aac5060fc535448658e8f6c6849cd56a31
ostrea <localhost@localhost>
2015-11-07 10:58:14 +0300

    Change who can access admin page, change mapping for it

--- src/main/java/my/ostrea/blog/configurations/WebSecurityConfig.java
+++ src/main/java/my/ostrea/blog/configurations/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
             .antMatchers("/").permitAll()
-            .antMatchers("/admin").access("hasRole('ROLE_USER') and hasRole('ROLE_ADMIN')")
+            .antMatchers("/admin").hasRole("ADMIN")
             .antMatchers("/css/**").permitAll()
             .anyRequest().authenticated()
             .and()
--- src/main/java/my/ostrea/blog/controllers/BaseController.java
+++ src/main/java/my/ostrea/blog/controllers/BaseController.java
@@ -3,14 +3,12 @@ package my.ostrea.blog.controllers;
 import my.ostrea.blog.models.MyUser;
 import my.ostrea.blog.models.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.security.Principal;
 import java.util.Optional;
 
 @Controller
@@ -34,8 +32,9 @@ public class BaseController {
         return "index";
     }
 
-    @RequestMapping(value = "admin", method = RequestMethod.GET)
+    @RequestMapping("admin")
     public String admin() {
         return "admin";
     }
+
 }
