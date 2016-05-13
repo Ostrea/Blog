af1af9732425dee6f598b61a0c9b88c03194573c
Ostrea <Ostrea@users.noreply.github.com>
2016-05-13 17:03:16 +0300

    Update dependencies versions and fix arisen problems.

--- src/main/java/my/ostrea/blog/configurations/WebSecurityConfig.java
+++ src/main/java/my/ostrea/blog/configurations/WebSecurityConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
@@ -21,7 +21,7 @@ import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import java.util.List;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
